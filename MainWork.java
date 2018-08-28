b753a5ae562941a3f000df872c74be39d6a4783b
TAREQ HASAN <akmhasan18@gmail.com>
2018-08-28 10:27:24 -0400

    home work 5, first commit

---
+++ MainWork.java
@@ -0,0 +1,20 @@
+package CoreJava;
+
+public class MainWork {
+
+    static int AIage;
+    static int AIid;
+    static String AIname;
+    static String AIintelligence;
+
+    static void disp(){
+        System.out.println("AI Name is: " +AIname);
+        System.out.println("AI Age is: " +AIage);
+        System.out.println("AI ID is: " +AIid);
+        System.out.println("AI Intelligence is: " +AIintelligence);
+    }
+
+
+
+
+}
---
+++ TestMainWork.java
@@ -0,0 +1,20 @@
+package CoreJava;
+
+public class TestMainWork {
+    public static void main(String[] args) {
+        MainWork.AIname = "Optimus Prime";
+        MainWork.AIage = 166;
+        MainWork.AIid = 10;
+        MainWork.AIintelligence = "Outstanding";
+
+        MainWork.AIname = "Bumble Bee";
+        MainWork.AIage = 109;
+        MainWork.AIid = 19;
+        MainWork.AIintelligence = "very good";
+
+        MainWork.disp();
+        System.out.println("------------------------------");
+        MainWork.disp();
+
+    }
+}
